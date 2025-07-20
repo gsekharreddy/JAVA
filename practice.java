f434ad73207f149d720217ef122dd49c35b1797c
gsekharreddy <sekhar.reddy.cse.2023@nist.edu>
2025-07-20 15:46:10 +0530

    upload

---
+++ practice.java
@@ -0,0 +1,37 @@
+ import java.util.*;
+
+ class A
+ {
+	int num;
+	A(int n)
+	{
+		num=this.n;
+	}
+	valA()
+	{
+		System.out.println("The value Stored is : "+this.num);
+	}
+ }
+
+ class B extends A
+{
+	String literall;
+	B(String lit)
+	{
+		literall=lit;
+	}
+	valB()
+	{
+		System.out.println("The value Stored is : "+this.literall);
+	}
+}
+
+public class practice
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		Scanner sc= new Scanner(System.in);	
+		
+	}
+}
